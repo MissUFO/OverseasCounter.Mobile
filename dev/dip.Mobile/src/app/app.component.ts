d487894933ed266e2e1df3c33e5f5fe078f0ba99
Tatiana Smetanina <smetaninatv@yandex.ru>
2018-10-01 21:15:20 +0300

    Small fixes

--- dev/dip.Mobile/src/app/app.component.ts
+++ dev/dip.Mobile/src/app/app.component.ts
@@ -42,6 +42,7 @@ export class AppComponent {
     this.initializeApp();
   }
 
+  location : UserGeoLocation;
   
   initializeApp() {
 
@@ -59,24 +60,82 @@ export class AppComponent {
 
   startBackgroundProcess()
   {
+
+      //stop background-fetch process (if it was started)
+      this.backgroundFetch.stop();
+
           const config: BackgroundFetchConfig = {
             stopOnTerminate: false, // Set true to cease background-fetch from operating after user "closes" the app. Defaults to true.
           };
 
           this.backgroundFetch.configure(config)
-             .then(() => {
-                 console.log('Background Fetch initialized');
+              .then(() => {
+
+                 this.saveDebugInfo('Background Fetch initialized');
+
+                 this.saveDebugInfo('Getting current location...');
+
+                 this.geolocation.getCurrentPosition().then((resp) => {
+
+                     if(resp!=null && resp!=undefined)
+                         {
+                            this.location.latitude = resp.coords.latitude;
+                            this.location.longitude = resp.coords.longitude;
+                            this.location.timestamp = resp.timestamp;
+
+                            this.saveDebugInfo(this.location.latitude + ', ' + this.location.longitude);
+                     }else
+                     {
+                         this.saveDebugInfo('Responce was undefined');
+                     }
+
+                 }).catch((error) => {
+
+                     this.saveDebugInfo('Error getting location. ' + error);
+
+                 }).then( resp => {
+
+                     this.setCurrentCountry(this.location); 
+                     this.saveDebugInfo(this.location.countryname + ' ' + this.location.address);
+
+                } );
 
                  this.backgroundFetch.finish();
 
              })
-             .catch(e => console.log('Error initializing background fetch', e));
+             .catch(e => this.saveDebugInfo('Error initializing background fetch' + e));
 
           // Start the background-fetch API. Your callbackFn provided to #configure will be executed each time a background-fetch event occurs. NOTE the #configure method automatically calls #start. You do not have to call this method after you #configure the plugin
           this.backgroundFetch.start();
 
           // Stop the background-fetch API from firing fetch events. Your callbackFn provided to #configure will no longer be executed.
-          this.backgroundFetch.stop();
+          //this.backgroundFetch.stop();
 
   }
+
+    saveDebugInfo(trace)
+    {
+        let debugInfo = '';
+
+        this.storage.get('debugInfo').then((val) => {
+            if(val!=null){
+                debugInfo = val;
+            }
+            debugInfo += '[' + new Date().toLocaleString() + ']';
+            debugInfo += ': '+ trace + '\r\n';
+            this.storage.set('debugInfo', debugInfo);
+        });
+
+    }
+
+
+    async setCurrentCountry(location: any)
+    {
+        let result = await this.geoService.getLocationAsync(location.latitude, location.longitude);
+
+        this.location.countryname = result.countryname;
+        this.location.address = result.address;
+
+        return this.location;
+    }
 }
